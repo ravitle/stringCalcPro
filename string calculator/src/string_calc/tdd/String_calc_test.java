018886a5fc3261e27a1907f3e938d210d69a056d
harelozya <ozhry9@gmail.com>
2013-11-24 13:29:51 +0200

    RED (ravit) : trying to add ""

---
+++ string calculator/src/string_calc/tdd/String_calc_test.java
@@ -0,0 +1,15 @@
+package string_calc.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class String_calc_test {
+
+	@Test //ravit
+	public void addEmptyStringTest() {
+		stringCalc str = new stringCalc();
+		int sum = str.Add("");
+		assertEquals(0,sum);
+	}
+}
---
+++ string calculator/src/string_calc/tdd/stringCalc.java
@@ -0,0 +1,8 @@
+package string_calc.tdd;
+
+public class stringCalc {
+
+	public int Add(String string) {
+		return -1;
+	}
+}
